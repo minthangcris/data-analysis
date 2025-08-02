b335245eaa0d21487238c9fc26c4a812dc9422bd
minhthang <minhthang.20041909@gmail.com>
2025-08-02 15:30:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ from datetime import datetime
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-import plotly.express as px
 import numpy as np
+import plotly.express as px
 
 # Cấu hình trang
 st.set_page_config(page_title="abc_manufacturing_data.csv", layout="wide")
